3f1e741e20bb9df79c8788f721b3d2d67c97d2c6
Farshad <farshad.ghanbari@hotmail.com>
2022-01-07 18:26:50 +0800

    initial setup

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAnSxPTqNdYvlbYymT5OUIhuLwHX90I3fQ',
+  authDomain: 'house-marketplace-3187c.firebaseapp.com',
+  projectId: 'house-marketplace-3187c',
+  storageBucket: 'house-marketplace-3187c.appspot.com',
+  messagingSenderId: '926205786466',
+  appId: '1:926205786466:web:676736bdfa1dc3f34bd43b',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore()
