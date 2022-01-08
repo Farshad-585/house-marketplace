3c0bf24bad859d9dded7da43ac5ed9bece3114d3
Farshad <farshad.ghanbari@hotmail.com>
2022-01-08 08:29:39 +0800

    registered users are added to database

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,7 +3,12 @@ import { Link, useNavigate } from 'react-router-dom'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
-import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
 import { db } from '../firebase.config'
 
 function SignUp() {
@@ -26,14 +31,25 @@ function SignUp() {
   const onSubmit = async (e) => {
     e.preventDefault()
     try {
-       const auth = getAuth()
-       const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-       const user = userCredential.user
-       updateProfile(user, { displayName: name })
-       navigate('/')
+      const auth = getAuth()
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      const user = userCredential.user
+      updateProfile(user, { displayName: name })
+
+      const formDataCopy = { ...formData }
+      delete formDataCopy.password
+      formDataCopy.timestamp = serverTimestamp()
+
+      await setDoc(doc(db, 'users', user.uid) , formDataCopy)
+
+      navigate('/')
     } catch (error) {
       console.log(error)
-    } 
+    }
   }
 
   return (
