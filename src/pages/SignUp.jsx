65430fbe8dcef506bf36f9e29496b3195530867b
Farshad <farshad.ghanbari@hotmail.com>
2022-01-08 08:21:48 +0800

    added user signup feature'

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,6 +3,9 @@ import { Link, useNavigate } from 'react-router-dom'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { db } from '../firebase.config'
+
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({
@@ -20,13 +23,26 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+       const auth = getAuth()
+       const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+       const user = userCredential.user
+       updateProfile(user, { displayName: name })
+       navigate('/')
+    } catch (error) {
+      console.log(error)
+    } 
+  }
+
   return (
     <>
       <div className='pageContainer'>
         <header>
           <p className='pageHeader'>Welcome Back!</p>
         </header>
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             className='nameInput'
             type='text'
